4874c8e3b4eef485c0c4ed4be4a96fba033b39c8
James Montemagno <james.montemagno@gmail.com>
2016-09-22 17:12:14 -0700

    Add forms sample app

---
+++ tests/app/ConnectivityTest/ConnectivityTestPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Xamarin.Forms;
+using Plugin.Connectivity;
+using System.Linq;
+
+namespace ConnectivityTest
+{
+	public partial class ConnectivityTestPage : ContentPage
+	{
+		
+
+		public ConnectivityTestPage()
+		{
+			InitializeComponent();
+
+		}
+
+		void HandleStart_Clicked(object sender, System.EventArgs e)
+		{
+			CrossConnectivity.Current.ConnectivityChanged += Current_ConnectivityChanged;
+			CrossConnectivity.Current.ConnectivityTypeChanged += Current_ConnectivityTypeChanged;
+		}
+
+		void HandleStop_Clicked(object sender, System.EventArgs e)
+		{
+			CrossConnectivity.Current.ConnectivityChanged -= Current_ConnectivityChanged;
+			CrossConnectivity.Current.ConnectivityTypeChanged -= Current_ConnectivityTypeChanged;
+
+		}
+
+		async void HandleIsConnected_Clicked(object sender, System.EventArgs e)
+		{
+			await DisplayAlert("Is Connected", CrossConnectivity.Current.IsConnected ? "YES" : "NO", "OK");
+		}
+
+		void Current_ConnectivityChanged(object sender, Plugin.Connectivity.Abstractions.ConnectivityChangedEventArgs e)
+		{
+			Device.BeginInvokeOnMainThread(async () =>
+			{
+				await DisplayAlert("Is Connected", e.IsConnected ? "YES" : "NO", "OK");
+
+			});
+		}
+
+		void Current_ConnectivityTypeChanged(object sender, Plugin.Connectivity.Abstractions.ConnectivityTypeChangedEventArgs e)
+		{
+			Device.BeginInvokeOnMainThread(async () =>
+			{
+				var stuff = string.Empty;
+				foreach (var i in e.ConnectionTypes)
+					stuff += "/n" + i.ToString();
+				
+				await DisplayAlert("Is Connected", (e.IsConnected ? "YES" : "NO") + stuff, "OK");
+
+			});
+		}
+	}
+}
---
+++ tests/app/iOS/AppDelegate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using Plugin.Connectivity;
+
+namespace ConnectivityTest.iOS
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+
+			var test = CrossConnectivity.Current.IsConnected;
+			global::Xamarin.Forms.Forms.Init();
+
+			LoadApplication(new App());
+
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
